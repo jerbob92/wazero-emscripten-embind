5dd5423a5658f794ed3923e98802fb65aca63c53
Jeroen Bobbeldijk <jeroen@klippa.com>
2023-12-04 20:50:05 +0100

    Add constructor and set function

--- js/array.go
+++ js/array.go
@@ -1,5 +1,15 @@
 package js
 
+import (
+	"fmt"
+	"unsafe"
+)
+
+type ArrayBuffer struct {
+	Buffer []uint8
+	Length int
+}
+
 type Int8Array struct {
 	Buffer []int8
 	Length int
@@ -35,7 +45,63 @@ type Float32Array struct {
 	Length int
 }
 
+func (obj *Float32Array) New(argTypes []string, args ...any) (any, error) {
+	if len(argTypes) == 3 {
+		switch v := args[0].(type) {
+		case Uint8Array:
+			sourceArray := unsafe.Slice((*float32)(unsafe.Pointer(&v.Buffer[0])), v.Length)
+			return &Float32Array{
+				Buffer: sourceArray[args[1].(int):(args[2].(int) * 4)],
+				Length: args[2].(int) / 4,
+			}, nil
+		case *Uint8Array:
+			sourceArray := unsafe.Slice((*float32)(unsafe.Pointer(&v.Buffer[0])), v.Length)
+			return &Float32Array{
+				Buffer: sourceArray[args[1].(int):(args[2].(int) * 4)],
+				Length: args[2].(int) / 4,
+			}, nil
+		case ArrayBuffer:
+			sourceArray := unsafe.Slice((*float32)(unsafe.Pointer(&v.Buffer[0])), v.Length)
+			return &Float32Array{
+				Buffer: sourceArray[args[1].(int):(args[2].(int) * 4)],
+				Length: args[2].(int) / 4,
+			}, nil
+		case *ArrayBuffer:
+			sourceArray := unsafe.Slice((*float32)(unsafe.Pointer(&v.Buffer[0])), v.Length)
+			return &Float32Array{
+				Buffer: sourceArray[args[1].(int):(args[2].(int) * 4)],
+				Length: args[2].(int) / 4,
+			}, nil
+		default:
+			return nil, fmt.Errorf("unknown source type: %T", v)
+		}
+	}
+
+	// @todo: implement other constructors.
+	return nil, fmt.Errorf("unknown constructor of length %d", len(argTypes))
+}
+
+func (obj *Float32Array) Set(input any) error {
+	switch v := input.(type) {
+	case Float32Array:
+		obj.Buffer = v.Buffer
+		obj.Length = v.Length
+		return nil
+	case *Float32Array:
+		obj.Buffer = v.Buffer
+		obj.Length = v.Length
+		return nil
+	}
+
+	return fmt.Errorf("set can only receive Float32Array or *Float32Array, got %T", input)
+}
+
 type Float64Array struct {
 	Buffer []float64
 	Length int
+}
+
+func (obj *Float64Array) New(argTypes []string, args ...any) (any, error) {
+	// @todo: implement me.
+	return Float64Array{}, nil
 }
